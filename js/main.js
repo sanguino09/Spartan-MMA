90a919b3ac87f287cfa5818c147478499a20fb04
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2024-10-07 23:24:12 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -174,3 +174,48 @@
 })(jQuery);
 
 
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+
+const table_madrid = document.getElementById('madrid_table');
+const table_toledo = document.getElementById('toledo_table');
+
+function madridHorario(){
+    console.log('madrid');
+    unfade(table_madrid);
+    table_toledo.style.display = 'none';
+}
+
+function toledoHorario(){
+    console.log('toledo');
+    unfade(table_toledo);
+    table_madrid.style.display = 'none';
+}
+
+
+
+
